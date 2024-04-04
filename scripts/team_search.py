c2c9dc0036e6674ae491dd99a85a16dc422a4b3d
marcelsadge <zjunhao@seas.upenn.edu>
2024-04-04 14:40:24 -0400

    Add python team scripts

---
+++ scripts/team_search.py
@@ -0,0 +1,39 @@
+import os
+import requests
+import json
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+## The Blue Alliance API Key
+GLOBAL_API_KEY = os.getenv('GLOBAL_API_KEY')
+
+## Max number of team pages
+MAX_PAGES = 20
+
+
+## Gets a list of all FRC teams and exports them to teams.txt
+def get_team_search_export_to_file():
+    page_count = 0
+    teams = dict()
+
+    f = open("../data/team_search.json", "w")
+
+    while (page_count < MAX_PAGES) :
+        response = requests.get('https://www.thebluealliance.com/api/v3/teams/' + str(page_count),
+                                headers={"X-TBA-Auth-Key": GLOBAL_API_KEY})
+        
+        api_data = response.json()
+
+        for team in api_data:
+            teams[str(team['team_number'])] = team['nickname']
+
+        page_count += 1
+    
+    json.dump(teams, f)
+    f.close()
+
+    
+# Run the program
+get_team_search_export_to_file()
